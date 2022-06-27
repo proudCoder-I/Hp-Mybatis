f7ba46c20ba35847cae4419e5dace6c6926ae492
hanpan <pan.han@okg.com>
2022-06-27 23:36:05 +0800

    Initial commit

---
+++ src/main/java/com/review/hpmybatis/HpMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.review.hpmybatis;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpMybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/review/hpmybatis/HpMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.hpmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
